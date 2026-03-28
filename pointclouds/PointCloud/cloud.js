7253bd5bb5d8b35d5be5cf897513e6233e31b707
dogru1 <dogukandogru061@gmail.com>
2026-03-28 18:31:28 -0500

    add TrueAI point cloud

---
+++ pointclouds/PointCloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 13247052,
+    "boundingBox": {
+        "lx": 671096.7000000001,
+        "ly": 3547068.04,
+        "lz": 186.6,
+        "ux": 672853.7600000001,
+        "uy": 3548825.1,
+        "uz": 1943.6600000000558
+    },
+    "tightBoundingBox": {
+        "lx": 671096.7000000001,
+        "ly": 3547068.04,
+        "lz": 186.6,
+        "ux": 672601.09,
+        "uy": 3548825.1,
+        "uz": 231.3
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 15.216586112976075,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
